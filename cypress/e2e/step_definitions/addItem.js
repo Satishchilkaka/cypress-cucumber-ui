ae1be3a0e1457efc8d216941dd5834f57ff8dd80
Satish <73182406+Satishchilkaka@users.noreply.github.com>
2023-04-13 07:51:40 -0400

    add new scenario

--- cypress/e2e/step_definitions/addItem.js
+++ cypress/e2e/step_definitions/addItem.js
@@ -19,11 +19,28 @@ import {
   });
 
   
-  Then("I should able to see the new item", () => {
+  Then("I should able to see the new Item Name 0001 item", () => {
     cy.get('p').should('exist')
     .and('be.visible')
   });
 
   
+  Given("homepage", () => {
+    cy.visit("/");
+  });
+  When("I enter a second new item name in input field", () => {
+    cy.get('[data-cy="itemName"]').type('Item Name 0002')
+    
+  });
   
+  When("I click add item button for second item", () => {
+    cy.get('[data-cy="addItem"]')
+    .click()
+  });
+
+  
+  Then("I should able to see the new Item Name 0002 item", () => {
+    cy.get('p').should('exist')
+    .and('be.visible')
+  });
   
